15d2ec701b35393209dfcedfffd778e224743606
Nagraj <ntadingi5@gmail.com>
2025-03-20 11:48:04 +0530

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Header(){
+
+    return (
+        <div className="h-16 border-b flex items-center px-2 py-3">
+            <h1 className="text-2xl font-bold">OTSI</h1>
+        </div>
+    )
+}
